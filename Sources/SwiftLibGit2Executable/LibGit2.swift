d4f2aa913ae70f4c1e37f1f1424660c808babd6f
ailion <1247008998@qq.com>
2020-01-12 18:06:12 +0800

    add LibGit2.swift

---
+++ Sources/SwiftLibGit2Executable/LibGit2.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by ailion on 2020/1/12.
+//
+
+import Clibgit2
+
+class LibGit2 {
+    
+    static func features() {
+        git_libgit2_features()
+    }
+    
+    static func initialize() {
+        git_libgit2_init()
+    }
+    
+    static func opts() {
+    }
+    
+    static func shutdown() {
+        git_libgit2_shutdown()
+    }
+    
+    static func version() -> LibGit2Version {
+        let major = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        let minor = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        let rev = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        git_libgit2_version(major, minor, rev)
+        
+        return LibGit2Version(major: major.pointee, minor: minor.pointee, rev: rev.pointee)
+        
+    }
+    
+    
+    
+}
---
+++ Sources/SwiftLibGit2Executable/LibGit2Version.swift
@@ -0,0 +1,5 @@
+struct LibGit2Version {
+    var major: Int32
+    var minor: Int32
+    var rev: Int32
+}
